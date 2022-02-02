37c2d2d6fabf4d08d894e5e752525a263e0c6261
avpetrov777 <98073948+avpetrov777@users.noreply.github.com>
2022-02-02 13:02:29 +0300

    refactor

--- linkedList.js
+++ linkedList.js
@@ -1,393 +1,366 @@
 class LinkedList {
-        constructor(elements) {
-          //создаёт список из массива элементов
-          function list(elements) {
-            let list = { value: elements[elements.length - 1], next: null };
-            let i = elements.length - 2;
+  constructor(elements) {
+    //создаёт список из массива элементов
+    this.head = null;
+    this.length = 0;
 
-            while (i >= 0) {
-              let tmp = Object.assign({}, list);
-              list.value = elements[i];
-              list.next = tmp;
-              i--;
-            }
+    for (let el of elements) {
+      this.add(el);
+    }
+  }
 
-            return list;
-          }
+  add(elem) {
+    //добавляет элемент в конец списка
+    let node = {
+      value: elem,
+      next: null,
+    };
 
-          this.length = elements.length;
-          this.head = list(elements);
-        }
+    if (this.head) {
+      if (this.head.next === null) {
+        this.head.next = node;
+      } else {
+        const prevNode = this.getNode(this.length - 1);
+        prevNode.next = node;
+      }
+    } else {
+      this.head = node;
+    }
+    this.length++;
+  }
 
-        add(elem) {
-          //добавляет элемент в конец списка
+  insert(i, elem) {
+    //i >= 0
+    //вставляет элемент в нужную позицию, если позиция больше чем есть элементов тогда в конец
+    // i элемент сдвигаем вправо, новый элемент занимает его место
+    console.log("insert ", i);
+    if (i < 0) {
+      throw new Error("Некорректный индекс");
+    }
 
-          if (this.length === 0) {
-            this.head.next = null;
-          } else {
-            this.head.next = Object.assign({}, this.head);
-          }
-          this.head.value = elem;
-          this.length++;
-        }
+    let node = {
+      value: elem,
+      next: null,
+    };
+    let prevNode = null;
+    let nextNode = null;
 
-        insert(i, elem) {
-          //i >= 0
-          //вставляет элемент в нужную позицию, если позиция больше чем есть элементов тогда в конец
-
-          let node = Object.assign({}, this.head);
-          let j = 0;
-          let prev = {};
-          let result = node;
-
-          if (i > this.length) {
-            i = this.length + 1;
-          }
-
-          while (j != i) {
-            prev = node;
-            node = node.next;
-            j++;
-          }
-          let tail = node;
-          let newNode = {
-            value: elem,
-            next: tail,
-          };
-
-          prev.next = newNode;
-          return result;
-        }
-
-        remove(i) {
-          //удаляет элемент на заданной позиции
-          //если индекс больше, чем размер списка, то выкинуть ошибку
-          //вернуть удалённый элемент
-          console.log(" length : ", this.length);
-          if (i >= this.length) {
-            throw new Error(" Индекс больше размеров списка ");
-          }
-
-          if (this.length === 0) {
-            throw new console.error("Список пуст");
-          }
-
-          let node = this.head;
-          let j = 0;
-          let prev = {};
-
-          if (i === 0) {
-            this.head = this.head.next;
-            this.length--;
-            return node.value;
-          }
-
-          while (j < i) {
-            prev = node;
-            node = node.next;
-            j++;
-          }
-          prev.next = node.next;
-          this.length--;
-          return node.value;
-        }
-
-        indexOf(elem) {
-          //возвращает индекс элемента в списке если он там есть, если нет то -1
-
-          let node = this.head;
-          let i = 0;
-          while (node.next !== null) {
-            if (node.value === elem) return i;
-            node = node.next;
-            i++;
-          }
-
-          return -1;
-        }
-
-        get(i) {
-          //возвращает элемент по индексу
-          if (i > this.length - 1 || i < 0) {
-            throw new Error("Некорректный индекс");
-          }
-          let node = this.head;
-          let j = 0;
-          while (j !== i) {
-            node = node.next;
-            j++;
-          }
-          return node.value;
-        }
-
-        getFirst() {
-          let node = this.head;
-          while (node.next !== null) {
-            node = node.next;
-          }
-          return node.value;
-        }
-
-        getLast() {
-          return this.head.value;
-        }
-
-        forEach(fn) {
-          //проходит по списку и вызывает для каждого элемента ф-цию fn
-          let node = this.head;
-          let j = 0;
-
-          while (j !== this.length) {
-            fn(node.value);
-            node = node.next;
-            j++;
-          }
-          return;
-        }
-
-        size() {
-          //возвращает размер списка
-          return this.length;
-        }
+    if (i === 0) {
+      node.next = this.head;
+      this.head = node;
+    } else {
+      if (i < this.length) {
+        prevNode = this.getNode(i - 1);
+        nextNode = prevNode.next;
+      } else {
+        prevNode = this.getNode(this.length - 1);
       }
 
-      //*********************
+      prevNode.next = node;
+      node.next = nextNode;
+    }
+    this.length++;
+    return;
+  }
 
-      class DoubleLinkedList {
-        //те же методы что и LinkedList
-        constructor(elements) {
-          this.head = null;
-          this.tail = null;
-          this.length = null;
+  remove(i) {
+    //удаляет элемент на заданной позиции
+    //если индекс больше, чем размер списка, то выкинуть ошибку
+    //вернуть удалённый элемент
 
-          if (elements) {
-            for (let el of elements) {
-              this.add(el);
-            }
-          }
-        }
+    if (i < 0 || i > this.length - 1) {
+      throw new Error("Некорректный индекс");
+    }
 
-        add(el) {
-          const node = {
-            value: el,
-            prev: null,
-            next: null,
-          };
+    let node = null;
 
-          if (this.head === null) {
-            this.head = this.tail = node;
-          } else {
-            node.prev = this.head;
-            this.head.next = node;
-            this.head = node;
-          }
+    if (i === 0) {
+      if (this.length === 1) {
+        const node = this.head;
+        this.head = null;
+        this.length = 0;
+        return node;
+      }
+      node = this.head;
+      this.head = node.next;
+    } else {
+      let prevNode = this.getNode(i - 1);
+      node = prevNode.next;
+      prevNode.next = node.next;
+    }
 
-          this.length++;
-        }
-        insert(i, el) {
-          //i >= 0
-          //вставляет элемент в нужную позицию, если позиция больше чем есть элементов тогда в конец
+    this.length--;
+    return node.value;
+  }
 
-          if (i < 0) {
-            return new Error("Некорректный индекс");
-          }
+  indexOf(elem) {
+    //возвращает индекс элемента в списке если он там есть, если нет то -1
 
-          const newNode = {
-            value: el,
-            prev: null,
-            next: null,
-          };
-          this.length++;
+    let node = this.head;
+    let i = 0;
 
-          if (i === 0) {
-            this.head.next = newNode;
-            newNode.prev = this.head;
-            this.head = newNode;
-            return;
-          }
-          
-          if (i === this.length-1) {
-            this.tail.prev = newNode;
-            newNode.next = this.tail;
-            this.tail = newNode;
-            return;
-          }
+    while (node) {
+      if (node.value === elem) {
+        return i;
+      }
+      node = node.next;
+      i++;
+    }
 
-          let j = 1;
-          let node = this.head.prev;
-          while (i > j) {
-            node = node.prev;
-            j++;
-          }
-  
-          let nodePrev = node.prev;
+    return -1;
+  }
 
-          node.prev = newNode;
-          newNode.prev = nodePrev;
-          nodePrev.next = newNode;
-          newNode.next = node;
-        }
-        remove(i) {
-          //удаляет элемент на заданной позиции
-          //если индекс больше, чем размер списка, то выкинуть ошибку
-          //вернуть удалённый элемент
+  getNode(i) {
+    // находим узел
+    console.log("getNode");
+    if (i < 0 || i > this.length - 1) {
+      throw new Error("индекс находится вне диапазона списка");
+    }
 
-          if (i > this.length - 1) {
-            throw new Error("Некорректный индекс");
-          }
+    let j = 0;
+    let node = this.head;
+    while (j < i) {
+      //   console.log("j , node.value : ", j,node.value);
+      node = node.next;
+      j++;
+    }
+    // console.log("node.value : ", node.value);
+    return node;
+  }
 
-          let node = this.tail;
-          if (i === 0) {
-            this.tail = this.tail.next;
-            this.tail.prev = null;
-          }
+  get(i) {
+    //возвращает элемент по индексу
+    return this.getNode(i).value;
+  }
 
-          if (i === this.length - 1) {
-            node = this.head;
-            this.head = this.head.prev;
-            this.head.next = null;
-          }
+  getFirst() {
+    return this.head.value;
+  }
 
-          let j = 1;
-          node = this.head.prev;
-          while (j > 0) {
-            if (j === i) break;
-            node = node.prev;
-            j++;
-          }
-          node.next.prev = node.prev;
-          node.prev.next = node.next;
+  getLast() {
+    return this.getNode(this.length - 1).value;
+  }
 
-          return node.value;
-        }
-        indexOf(el) {
-          //возвращает индекс элемента в списке если он там есть, если нет то -1
-          let j = 0;
-          let node = this.head;
-          while (j < this.length) {
-            if (node.value === el) {
-              return j;
-            }
-            node = node.prev;
-            j++;
-          }
-          return -1;
-        }
-        get(i) {
-          //возвращает элемент по индексу
-          if (i > this.length - 1 || i < 0) {
-            throw new Error("Некорректный индекс");
-          }
+  forEach(fn) {
+    //проходит по списку и вызывает для каждого элемента ф-цию fn
+    let node = this.head;
 
-          let j = 0;
-          let node = this.head;
-          while (j !== i) {
-            node = node.prev;
-            j++;
-          }
-          return node.value;
-        }
-        getFirst() {
-          return this.head;
-        }
-        getLast() {
-          return this.tail;
-        }
-        forEach(fn) {
-          let i = 0;
-          let node = this.head;
-          while (i < this.length) {
-            fn(node.value);
-            node = node.prev;
-            i++;
-          }
-        }
-        size() {
-          return this.length;
-        }
+    while (node) {
+      fn(node.value);
+      node = node.next;
+    }
+  }
+
+  size() {
+    //возвращает размер списка
+    return this.length;
+  }
+}
+
+//*********************
+
+class DoubleLinkedList {
+  //те же методы что и LinkedList
+  constructor(elements) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+
+    if (elements) {
+      for (let el of elements) {
+        this.add(el);
+      }
+    }
+  }
+
+  add(el) {
+    this.insert(this.length, el);
+  }
+
+  insert(i, el) {
+    //i >= 0
+    //вставляет элемент в нужную позицию, если позиция больше чем есть элементов тогда в конец
+    console.log("insert : ", i);
+    if (i < 0) {
+      throw new Error("Некорректный индекс");
+    }
+
+    if (i > this.length - 1) {
+      const node = {
+        value: el,
+        prev: null,
+        next: null,
+      };
+
+      if (this.tail) {
+        node.prev = this.tail;
+        this.tail.next = node;
+        this.tail = node;
+      } else {
+        this.head = this.tail = node;
       }
 
+      this.length++;
+      return;
+    }
 
-      class Stack {
-        constructor() {
-          this.head = null;
-          this.length = 0;
-        }
+    let newNode = {
+      value: el,
+      prev: null,
+      next: null,
+    };
 
-        push(elem) {
-          //добавляет в стэк элемент
-          const node = {
-            value: elem,
-            prev: null
-          }
+    if (i === 0) {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    } else {
+      let prevNode = this.getNode(i - 1);
+      let nextNode = prevNode.next;
+      console.log("prev,next : ", prevNode.value, nextNode.value);
+      prevNode.next = newNode;
+      newNode.next = nextNode;
+      nextNode.prev = newNode;
+      newNode.prev = prevNode;
+    }
 
-          if (this.length === 0) {
-            this.head = node;
-          } else {
-            node.prev = this.head;
-            this.head = node;
-          }
-          this.length++;
-        }
-        pop() {
-          //достает верхний элемент и возвращает его, если стэк пустой то ошибку выкинуть
-          if (this.length === 0) {
-            return new Error("Стэк пуст");
-          }
-          const res = this.head.value;
-          this.head = this.head.prev;
-          this.length--;
-          return res;
-        }
-        peak() {
-          //просто возвращает верхний элемент, но он остается в стэке
-          return this.head.value;
-        }
-        size() {
-          //возвращает размер стэка
-          return this.length;
-        }
+    this.length++;
+  }
+
+  remove(i) {
+    //удаляет элемент на заданной позиции
+    //если индекс больше, чем размер списка, то выкинуть ошибку
+    //вернуть удалённый элемент
+    if (i < 0) {
+      throw new Error("Некорректный индекс");
+    }
+    const node = this.getNode(i);
+
+    if (node === this.head) {
+      if (this.length === 1) {
+        this.head = null;
+        this.length = 0;
+        return node;
       }
+      this.head = this.head.next;
+      this.head.prev = null;
+    } else if (node === this.tail) {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+    } else {
+      let prevNode = node.prev;
+      let nextNode = node.next;
+      prevNode.next = nextNode;
+      nextNode.prev = prevNode;
+    }
+    this.length--;
+    return node.value;
+  }
 
-      class Queue {
-        constructor() {
-          this.head = null;
-          this.tail = null;
-          this.length = 0;
-        }
+  indexOf(el) {
+    //возвращает индекс элемента в списке если он там есть, если нет то -1
 
-        enqueue(elem) {
-          //добавляет элемент в очередь
-          const node = {
-            value: elem,
-            prev: null,
-            next: null
-          }
-
-          if (this.length === 0) {
-            this.head = this.tail = node;
-          } else {
-            node.prev = this.head;
-            this.head.next = node;
-            this.head = node;
-          }
-
-          this.length++;
-        }
-        dequeue() {
-          //достает элемент из очереди
-          //если очередь пустая то бросить ошибку
-          if(this.length === 0) {
-            return new Error("Очередь пуста");
-          }
-          const res = this.tail.value;
-          this.tail = this.tail.next;
-          this.tail.prev = null;
-          this.length--;
-          return res;
-        }
-        size() {
-          //возвращает размер очереди
-          return this.length;
-        }
+    let node = this.head;
+    let i = 0;
+    while (node) {
+      if (node.value === el) {
+        return i;
       }
+      node = node.next;
+      i++;
+    }
+    return -1;
+  }
+
+  getNode(i) {
+    // даёт элемент по индексу
+    console.log("getNode");
+    if (i < 0 || i > this.length - 1) {
+      throw new Error("Индекс находится вне диапазона списка");
+    }
+
+    let node = this.head;
+    let j = 0;
+    while (j <= this.length) {
+      if (i === j) {
+        return node;
+      }
+      node = node.next;
+      j++;
+    }
+  }
+
+  get(i) {
+    //возвращает элемент по индексу
+    return this.getNode(i).value;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    return this.tail;
+  }
+
+  forEach(fn) {
+    let node = this.head;
+    while (node) {
+      fn(node.value);
+      node = node.next;
+    }
+  }
+
+  size() {
+    return this.length;
+  }
+}
+
+class Stack {
+  constructor(elements) {
+    this.stack = new DoubleLinkedList(elements);
+  }
+
+  push(elem) {
+    //добавляет в стэк элемент
+    this.stack.add(elem);
+  }
+  pop() {
+    //достает верхний элемент и возвращает его, если стэк пустой то ошибку выкинуть
+    if (this.stack.length === 0) {
+      return new Error("пустой стэк");
+    }
+    return this.stack.remove(this.stack.length - 1);
+  }
+  peak() {
+    //просто возвращает верхний элемент, но он остается в стэке
+    return this.stack.getLast().value;
+  }
+  size() {
+    //возвращает размер стэка
+    return this.stack.length;
+  }
+}
+
+class Queue {
+  constructor(elements) {
+    this.queue = new DoubleLinkedList(elements);
+  }
+
+  enqueue(elem) {
+    //добавляет элемент в очередь
+    this.queue.add(elem);
+  }
+  dequeue() {
+    //достает элемент из очереди
+    //если очередь пустая то бросить ошибку
+    if (this.queue.length === 0) {
+      return new Error("Очередь пуста");
+    }
+    return this.queue.remove(0);
+  }
+  size() {
+    //возвращает размер очереди
+    return this.queue.length;
+  }
+}
